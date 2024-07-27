520d4e3fedf871fd66912a9ce06df1f7df6ba982
Paul Garton <paulgarton1984@hotmail.com>
2024-07-27 18:36:29 +0100

    create react app, add firebase dependencies and initialize firebase app

---
+++ firebase/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseApp = initializeApp({
+  apiKey: "AIzaSyAOITHn47GdkY36we0kS4Ch9qYLH7rkql4",
+  authDomain: "auth-development-617c7.firebaseapp.com",
+  projectId: "auth-development-617c7",
+  storageBucket: "auth-development-617c7.appspot.com",
+  messagingSenderId: "754251437431",
+  appId: "1:754251437431:web:71a76d675da5ceab68bd72",
+});
+
+export const auth = getAuth(firebaseApp);
